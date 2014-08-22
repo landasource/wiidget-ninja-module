636f7bf752fe2ae4fec67e8d27e0ec0a4bbf0fe1
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2014-08-22 09:40:08 +0200

    Solve java warnings

--- src/main/java/org/landa/wiidget/ninja/NinjaWiidgetValidator.java
+++ src/main/java/org/landa/wiidget/ninja/NinjaWiidgetValidator.java
@@ -2,8 +2,6 @@ package org.landa.wiidget.ninja;
 
 import java.util.List;
 
-import ninja.validation.Validation;
-
 import org.landa.wiidget.Wiidget;
 import org.landa.wiidget.engine.ObjectFactory;
 import org.landa.wiidget.validation.DefaultWiidgetValidator;
@@ -13,20 +11,24 @@ import com.google.inject.Inject;
 
 public class NinjaWiidgetValidator extends DefaultWiidgetValidator {
 
-	private final Validation validation;
+    //  private final Validation validation;
 
-	@Inject
-	public NinjaWiidgetValidator(final Validation validation, final ObjectFactory objectFactory) {
-		super(objectFactory);
-		this.validation = validation;
-	}
+    //    @Inject
+    //    public NinjaWiidgetValidator(final Validation validation, final ObjectFactory objectFactory) {
+    //        super(objectFactory);
+    //        this.validation = validation;
+    //    }
 
-	@Override
-	public List<ValidationError> validate(final Wiidget wiidget) {
-		// TODO
+    @Override
+    public List<ValidationError> validate(final Wiidget wiidget) {
 
-		return super.validate(wiidget);
+        return super.validate(wiidget);
 
-	}
+    }
+
+    @Inject
+    public NinjaWiidgetValidator(final ObjectFactory objectFactory) {
+        super(objectFactory);
+    }
 
 }
